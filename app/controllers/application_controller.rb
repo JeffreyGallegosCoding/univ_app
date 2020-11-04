38eac5ed67ec723f42eaa6f0c90ddd1a4ad208d6
JeffreyGallegosCoding <jeffreygallegos01@gmail.com>
2020-11-04 16:12:34 -0700

    Added session create and destroy functionality for students

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # Identifies these as helper methods so they can be used erey wer
+  helper_method :current_user, :logged_in?
+  # If a session variable is set then it will find that student id based on their id
+  # Good way to check if there is a logged in user
+  def current_user
+    # memoization ||= this means that it will only perform this query if the user does not
+    # exist
+    @current_user ||= Student.find(session[:student_id]) if session[:student_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,26 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    student = Student.find_by(email: params[:sessions][:email].downcase)
+    # Be sure to include the first student in order verify that the student object is
+    # valid and exists in the database
+    if student && student.authenticate(params[:sessions][:password])
+      # Set the session variable (what is stored as a cookie in the browser)
+      session[:student_id] = student.id
+      flash[:notice] = "You have successfully logged in"
+      redirect_to student
+    else
+      # Flash.now stays for only the page that it is on
+      flash.now[:notice] = "Something was wrong with your login info"
+      render 'new'
+    end
   end
 
   def destroy
-
+    # Sets your previous session to nil
+    session[:student_id] = nil
+    flash[:notice] = "You have successfully logged out"
+    redirect_to root_path
   end
 
 end
